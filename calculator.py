4aa270c766bfa20c852fe9ebc45bb6a6d7667ffb
ShahadFawazShadad155737 <latifahfaed1975@gmail.com>
2025-11-18 11:47:21 +0300

    Comparison

--- calculator.py
+++ calculator.py
@@ -21,4 +21,14 @@ while True:
         print("you have chosen something I don't know")
 
     
+    if num1 > num2: 
+        print(num1, "is greater than", num2)
+
+    elif num1 < num2: 
+        print(num1, "is smaller than", num2)
+
+    else:
+        print(num1, "is equal to", num2)
+
     
+   
