45c2b9977160f51f51d715cac7f769be997ded7c
ShahadFawazShadad155737 <latifahfaed1975@gmail.com>
2025-11-18 11:51:32 +0300

    Check if the numbers are positive or negative

--- calculator.py
+++ calculator.py
@@ -31,4 +31,20 @@ while True:
         print(num1, "is equal to", num2)
 
     
-   
+    print("\nNumber status:")
+    if num1 > 0:
+        print(num1, "is positive")
+    elif num1 < 0:
+        print(num1, "is negative")
+    else:
+        print(num1, "is zero")
+
+    if num2 > 0:
+        print(num2, "is positive")
+    elif num2 < 0:
+        print(num2, "is negative")
+    else:
+        print(num2, "is zero")
+
+    
+    
