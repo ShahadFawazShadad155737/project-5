a942fa6b21de86a97f7a46c9665b6e6ce956b566
ShahadFawazShadad155737 <latifahfaed1975@gmail.com>
2025-11-18 11:40:47 +0300

    Performing the calculation

--- calculator.py
+++ calculator.py
@@ -5,4 +5,20 @@ while True:
     choice = input("choose [*, -, +, /]: ")
 
     
-  
+    if choice == '*':
+        print('multiplying =', num1 * num2)
+
+    elif choice == '-':
+        print('minus =', num1 - num2)
+
+    elif choice == '+':
+        print('plus =', num1 + num2)
+
+    elif choice == '/':
+        print('divide =', num1 / num2)
+
+    else:
+        print("you have chosen something I don't know")
+
+    
+    
